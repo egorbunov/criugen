588299fdbe6ee510cfe8525a351f4f02f162cf1f
Egor Gorbunov <egor-mailbox@ya.ru>
2016-08-19 14:21:52 +0300

    Add Makefile; fix proto dependencies; ...

--- restorer/compile_proto.sh
+++ restorer/compile_proto.sh
@@ -2,10 +2,22 @@ CRIU_DIR=~/dev/criu
 SOURCE_OUT=./src/proto
 HEADER_OUT=./include/proto
 
+rm -rf "$SOURCE_OUT" && \
+rm -rf "$HEADER_OUT" && \
 mkdir -p "$SOURCE_OUT" && \
 mkdir -p "$HEADER_OUT"
 
-declare -a PROTOS=("pstree" \
+declare -a PROTOS=("opts" \
+	           "core-x86" \
+	           "core-arm" \
+	           "core-aarch64" \
+	           "core-ppc64" \
+	           "rlimit" \
+	           "timer" \
+	           "creds" \
+	           "siginfo" \
+	           "fown" \
+	           "pstree" \
 	           "fs" \
 	           "fdinfo" \
 	           "core" \
@@ -19,4 +31,7 @@ for f in "${PROTOS[@]}"; do
 	protoc -I=${CRIU_DIR}/images --cpp_out="$SOURCE_OUT" ${CRIU_DIR}/images/${f}.proto
 done
 
-mv ${SOURCE_OUT}/*.h ${HEADER_OUT}
+mv ${SOURCE_OUT}/*.h ${HEADER_OUT} && \
+find ${SOURCE_OUT} -name "*.cc" -print0 | \
+	xargs -i@ -0 bash -c 'FILE=@; mv "$FILE" "${FILE%.*}.cpp"' && \
+echo OK
---
+++ restorer/src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello, stranger!" << std::endl;
+	return 0;
+}
