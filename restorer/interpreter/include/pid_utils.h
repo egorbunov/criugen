2823bdb7d35e334abbb42e3fc23e31c94dbf206f
Egor Gorbunov <egor-mailbox@ya.ru>
2016-10-25 01:18:58 +0300

    Cleanup branch for reports

--- restorer/interpreter/include/pid_utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef PID_UTILS_H_INCLUDED__
-#define PID_UTILS_H_INCLUDED__
-
-#include <sys/types.h>
-
-int fork_pid(pid_t);
-
-#endif
--- restorer/interpreter/run.sh
+++
@@ -1,5 +0,0 @@
-make clean all && \
-sudo rm -f interpreter_log.log && \
-sudo bin/restorer bin/cmds.json && \
-sleep 1 && \
-sudo cat interpreter_log.log
