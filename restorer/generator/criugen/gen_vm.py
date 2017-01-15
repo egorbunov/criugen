876a9b00586bb1be63cc0e414e074bfd84e50a73
Egor Gorbunov <egor-mailbox@ya.ru>
2017-01-16 02:04:24 +0300

    Small changes (temporary comment with details added)

--- restorer/generator/criugen/gen_vm.py
+++ restorer/generator/criugen/gen_vm.py
@@ -11,4 +11,26 @@ def handle_vm(app):
     :return: dictionary with structure: {pid: [command]}, i.e. for every pid
     you'll have list of commands
     """
+
+    """
+    What interpreter should do?
+
+    1) Find all COW (private) mappings and:
+       - mmap in parent before forking child
+       - populate with pages in parent before forking child
+       - in every child, which inherited COW-mapping, fix pages (change some of them),
+       which were populated in parent
+       - after switching to restorer context stage remap! mappings
+
+    2) MAP_SHARED + backed by file; Open files, which are needed by VMAs with MAP_SHARED
+       and shmid != 0 (backed by file) and mmap them as usual AFTER transition to restorer
+       context (do we need to populate this areas with pages at the end?)
+
+    3) MAP_SHARED | MAP_ANON. Open anonymous file with memfd_open syscall, mmap opened file
+       and populate it with pages from shmem_pages.img (or something like this). Close the
+       mapping afterwards and make other processes to open this file (it seems to be convenient
+       to send this fd using unix socket, so?)
+    """
+
     return {p.pid: [] for p in app.processes}
+
