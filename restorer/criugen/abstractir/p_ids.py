517f7823ddf51b6e5df2f6bf1feed6eb1b60ed7b
Egor Gorbunov <egor-mailbox@ya.ru>
2017-05-23 18:41:21 +0300

    Add empty methods for groups, sessions and user creds resources initialization

---
+++ restorer/criugen/abstractir/p_ids.py
@@ -0,0 +1,20 @@
+"""
+Initialization of credential resources: groups, sessions, user ids and so on
+"""
+
+from model import crdata
+from pstree import ProcessTreeConcept
+from resource_concept import RegularFileConcept, ResourceConcept, SharedMemConcept
+from resource_handles import *
+
+
+def _init_groups_resource():
+    pass
+
+
+def _init_sessions_resource():
+    pass
+
+
+def _init_user_creds_resource():
+    pass
