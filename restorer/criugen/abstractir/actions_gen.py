dec37355724e7404cc3ce4093013008682b4cbe4
Egor Gorbunov <egor-mailbox@ya.ru>
2017-05-26 21:33:24 +0300

    Add generator stub

---
+++ restorer/criugen/abstractir/actions_gen.py
@@ -0,0 +1,20 @@
+""" Public interface for action list generation
+"""
+
+import model.crdata as crdata
+import concept
+import closure
+
+
+def generate_actions(application):
+    """ Generates list of abstract actions for given process
+    tree restoration
+    
+    :param application:  task to be restored
+    :type application: crdata.Application
+    :return: list of high-order actions
+    """
+
+    process_tree = concept.init_conceptual_process_tree(application)
+
+    return []
--- restorer/criugen/generator/gen.py
+++ restorer/criugen/generator/gen.py
@@ -1,12 +1,5 @@
-import itertools
-
-import command
-import actions_graph
-
 from model import crdata
-from model.reg_file import RegularFilesProvider
-from model.pstree import ProcessTree
-from model.vm import SharedVmas, PrivateVmas
+from abstractir import actions_gen
 
 
 def generate_program(app):
@@ -17,15 +10,5 @@ def generate_program(app):
     :return: program == list of commands, each command is dictionary (see command file)
     """
 
-    process_tree = ProcessTree(app.processes)
-    shared_vmas_provider = SharedVmas(app.processes, files=app.regular_files)
-    private_vmas_provider = PrivateVmas(app.processes, files=app.regular_files)
-    reg_files_provider = RegularFilesProvider(app.processes, app.regular_files,
-                                              dep_providers=[shared_vmas_provider, private_vmas_provider])
-
-    act_graph = actions_graph.build_action_graph(process_tree, [
-        process_tree,
-        reg_files_provider,
-        private_vmas_provider,
-        shared_vmas_provider,
-    ])
+    # generating high-order action list
+    actions = actions_gen.generate_actions(app)
