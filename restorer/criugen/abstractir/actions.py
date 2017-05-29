1d39ae624427a4b06e2d3322e907e8705d96090b
Egor Gorbunov <egor-mailbox@ya.ru>
2017-05-29 13:46:59 +0300

    Switch from namedtuple to DataClass for actions structures

--- restorer/criugen/abstractir/actions.py
+++ restorer/criugen/abstractir/actions.py
@@ -2,32 +2,32 @@
 """
 
 from collections import namedtuple
-
-ForkProcessAction = namedtuple('ForkProcessAction', [
-    'parent',  # process, which forks child
-    'child'    # child, which is being forked
-])
-
-CreateResourceAction = namedtuple('CreateResourceAction', [
-    'process',   # process, which creates resource
-    'resource',  # resource, which is being created
-    'handles'    # list of handles; contains more than one handle in
-                 # case of multi-handle resource
-])
-
-ShareResourceAction = namedtuple('ShareResourceAction', [
-    'process_from',  # process, which has the (resource, handle_from) in it
-    'process_to',    # process, with which resource is shared
-    'resource',      # resource, which is being shared
-    'handle_from',   # handle of the resource in the process, which has the resource
-    'handle_to'      # handle, which is going to point to the resource after share
-                     # action within `process_to` process
-])
+from pyutils.dataclass import DataClass
 
 
-RemoveResourceAction = namedtuple('RemoveResourceAction', [
-    'process',   # process, which executes the action of removing the resource
-    'resource',  # resource, handle to which is being removed from the process
-    'handle'     # handle, which is being released, so (resource, handle) pair is
-                 # removed from `process` as an effect of this action
-])
+class ForkProcessAction(DataClass):
+    parent = "process, which forks child"
+    child = "child, which is being forked"
+
+
+class CreateResourceAction(DataClass):
+    process = "process, which creates resource"
+    resource = "resource, which is being created"
+    handles = "ist of handles; contains more than one handle " \
+              "in case of multi-handle resource"
+
+
+class ShareResourceAction(DataClass):
+    process_from = "process, which has the (resource, handle_from) in it"
+    process_to =  "process, with which resource is shared"
+    resource = "resource, which is being shared"
+    handle_from = "handle of the resource in the process, which has the resource"
+    handle_to = "handle, which is going to point to the resource after " \
+                "share action within `process_to` process"
+
+
+class RemoveResourceAction(DataClass):
+    process = "process, which executes the action of removing the resource"
+    resource = "resource, handle to which is being removed from the process"
+    handle = "handle, which is being released, so (resource, handle) pair " \
+             "is removed from `process` as an effect of this action"
